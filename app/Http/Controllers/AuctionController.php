1385c554b84ba4c79411678da96dc16eae258d3a
PedroVidal <pedrovidal2004@gmail.com>
2023-11-18 16:17:45 +0000

    user profile information dump

--- app/Http/Controllers/AuctionController.php
+++ app/Http/Controllers/AuctionController.php
@@ -9,7 +9,7 @@ use App\Models\Auction;
 class AuctionController extends Controller
 {
     
-    public function show()
+    public function list()
     {
       $auctions = Auction::all();
       return view('pages.auctionsListing', ['auctions' => $auctions]);
