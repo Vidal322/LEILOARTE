24cc9800e8c1eea7f68f58a761fbb50bc9d917dc
andreiasilva66 <up202108769@g.uporto.pt>
2023-12-02 16:06:20 +0000

    faqs are done but are ugly

--- resources/views/partials/faqCard.blade.php
+++ resources/views/partials/faqCard.blade.php
@@ -5,7 +5,7 @@
         {{-- if admin--}}
         @if (Auth::check() && Auth::user()->type == 'admin')
             <a class="button" id="editFaqButton" href="{{ route('editFAQForm', ['id' => $faq->id]) }}"> Edit </a>
-            <form method="POST" action="{{ route('deleteFAQ', ['id' => $faq->id]) }}">
+            <form method="POST" class="button" action="{{ route('deleteFAQ', ['id' => $faq->id]) }}">
                 @csrf
                 @method('POST')
                 <button type="submit">Delete</button>
