a45d8a1860114c01d292db5836f08609c122633e
andreiasilva66 <up202108769@g.uporto.pt>
2023-12-02 23:20:29 +0000

    added more info

--- resources/views/pages/aboutUs.blade.php
+++ resources/views/pages/aboutUs.blade.php
@@ -3,11 +3,19 @@
 @section('content')
 <div class="about_us">
     <h2> Welcome to LeiloArte – Where Art Finds Its True Value! </h2>
-    <h4> At LeiloArte, we are passionate about connecting art enthusiasts </br>
+    <p> At LeiloArte, we are passionate about connecting art enthusiasts </br>
          with exceptional pieces that inspire and captivate. Our mission is to create </br>
           a vibrant and accessible online auction platform dedicated exclusively to art, </br>
           providing a seamless experience for both buyers and sellers.
-    </h4>
+    </p>
+    <h2>Our Commitment: </h2>
+    <h4> Curate a diverse collection of high-quality artworks from talented artists worldwide. </h4>
+    <h4> Foster a community that values the beauty, emotion, and cultural significance of art.</h4>
+    <h4> Ensure transparency, security, and trust in every transaction on our platform.</h4>
+    <p>
+        Join us in celebrating the world of art and discover unique masterpieces that resonate with your soul. </br> 
+        LeiloArte is more than an auction site; it's a canvas where creativity meets appreciation.
+    </p>
 </div>
 <h4> Contact Information: </h4>
 <p> Have Questions or Need Assistance? We're Here to Help! </p>
